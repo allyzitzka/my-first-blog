cd3edd5f5970045e03bd5d4361e3a7ba5192335b
Ally <allykitkaat@gmail.com>
2017-08-02 11:23:24 -0700

    My Django Girls app, second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o3&&ff+=co72p=j1*#n-%)(0##s2!2pwg&-v-(s7ei-hn2uhj^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<Girls Who Code>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'allyzitzka.pythonanywhere.com']
 
 
 # Application definition
